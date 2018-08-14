c5b1781346dfd1d7551f59a607bd40830f830a2e
John Casey <jdcasey@commonjava.org>
2018-08-14 14:32:14 -0500

    pull the JS local and change src refs

---
+++ js/update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js > jquery.min.js
+curl https://raw.githubusercontent.com/datejs/Datejs/master/build/date-en-US.js > date-en-US.js
